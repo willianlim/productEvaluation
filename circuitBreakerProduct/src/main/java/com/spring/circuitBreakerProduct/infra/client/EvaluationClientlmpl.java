f8ee556d5c9f684984c7be460db0c5acec8189d5
Willian-code <willianrosendodelima@outlook.com>
2022-10-26 17:26:58 -0300

    adding annotations for circuit breaker and cache

--- circuitBreakerProduct/src/main/java/com/spring/circuitBreakerProduct/infra/client/EvaluationClientlmpl.java
+++ circuitBreakerProduct/src/main/java/com/spring/circuitBreakerProduct/infra/client/EvaluationClientlmpl.java
@@ -2,16 +2,14 @@ package com.spring.circuitBreakerProduct.infra.client;
 
 import com.spring.circuitBreakerProduct.client.evaluation.EvaluationClient;
 import com.spring.circuitBreakerProduct.client.evaluation.EvaluationModel;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class EvaluationClientlmpl implements EvaluationClient {
@@ -25,11 +23,14 @@ public class EvaluationClientlmpl implements EvaluationClient {
             .encode()
             .toUriString();
 
+    private final Map<Long, List<EvaluationModel>> CACHE = new HashMap<>();
+
     public EvaluationClientlmpl(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
 
     @Override
+    @CircuitBreaker(name = "avaliacaoCB", fallbackMethod = "searchAllProductsInCache")
     public List<EvaluationModel> searchAllProducts(Long productId) {
         final List<EvaluationModel> evaluation = executeRequest(productId);
         return (evaluation);
@@ -48,6 +49,15 @@ public class EvaluationClientlmpl implements EvaluationClient {
             logger.error("Error when fetching reviews.");
             throw e;
         }
+
+        logger.info("Alimentando cache");
+        CACHE.put(productId, Arrays.asList(evaluation));
+
         return (Arrays.asList(evaluation));
+    }
+
+    private List<EvaluationModel> searchAllProductsInCache(Long productId, Throwable e) {
+        logger.info("Buscando no cache");
+        return CACHE.getOrDefault(productId, new ArrayList<>());
     }
 }
